7d6707bd80621779799f5af06ed4701a31acf327
priorbg <brunoprior@gmail.com>
2018-03-25 20:30:04 -0300

    WebScrappingTest
    .NET app to do web scrapping using MongoDB as database

---
+++ WebScrappingTest/WebScrappingTest/SeleniumConfigurations.cs
@@ -0,0 +1,9 @@
+﻿namespace WebScrappingTest
+{
+    public class SeleniumConfigurations
+    {
+        public string CaminhoDriverFirefox { get; set; }
+        public string UrlPaginaClassificacaoNBA { get; set; }
+        public int Timeout { get; set; }
+    }
+}
